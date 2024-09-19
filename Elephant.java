d903ee79f48201bff3d268cba2cd97a29783b587
ryxnh <349739326@gapps.yrdsb.ca>
2024-09-19 12:50:44 -0400

    partial part one

---
+++ Elephant.java
@@ -0,0 +1,44 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1); 
+        }
+        
+        eat(); 
+        
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class); 
+            MyWorld world = (MyWorld) getWorld(); 
+            world.createApple();
+            
+        }
+        
+    }
+    
+    
+}
--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,21 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant,300, 300);
+        
+        createApple(); 
+        
     }
+    
+    
+    public void createApple() 
+    {
+         Apple apple = new Apple();
+         int x = Greenfoot.getRandomNumber(600);
+         int y = 0; 
+        addObject(apple, x, y); 
+    }
+    
 }
